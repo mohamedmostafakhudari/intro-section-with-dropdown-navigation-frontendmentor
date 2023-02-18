b5f4465226e3452d9e64e7c54a70a30e3c771c10
Mohamed <mohamedmostafakhudari@gmail.com>
2023-02-18 13:25:25 +0200

    first commit

---
+++ TODO.md
@@ -0,0 +1,5 @@
+[] - View the relevant dropdown menus on desktop and mobile when interacting with the navigation links
+
+[] - View the optimal layout for the content depending on their device's screen size
+
+[] - See hover states for all interactive elements on the page
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        almostWhite: "hsl(0, 0%, 98%)",
+        mediumGray: "hsl(0, 0%, 41%)",
+        almostBlack: "hsl(0, 0%, 8%)",
+      },
+      fontSize: {
+        primary: "18px",
+      },
+      fontFamily: {
+        body: ["'Epilogue'", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
