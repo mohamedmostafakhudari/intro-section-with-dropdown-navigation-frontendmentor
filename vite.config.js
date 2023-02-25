ef35142a511ac6e2c5c93489f9ef14f69f22d759
Mohamed <mohamedmostafakhudari@gmail.com>
2023-02-25 16:35:23 +0200

    edit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/intro-section-with-dropdown-navigation-frontendmentor/",
   plugins: [react()],
-  server: {
-    host: true,
-  },
 });
